b7104677821965363bf8fcfd4d2576010b527767
Babacar Ly <babacarly111@gmail.com>
2019-08-31 02:47:31 +0000

    Home

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        ooooooo
         <Navbar/>
         <TourList/>
         <Router>
